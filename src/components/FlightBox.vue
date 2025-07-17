54627064dcb0ace16e8b81fd838759023ee23eb7
jakeg96 <jakepgreen6@gmail.com>
2025-07-17 16:24:23 +1000

    Flightcard and layout basics

---
+++ src/components/FlightBox.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import type { Flight } from "@/types/flight"
+
+interface Props {
+  flight: Flight
+}
+
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+  <article class="flightCard">
+    <div class="departingInfo">
+      <h1>{{ flight.departure.iata }}</h1>
+      <p>{{ flight.departure.airport }}</p>
+      <p>{{ flight.departure.scheduled }}</p>
+      <p>Gate: {{ flight.departure.gate }}</p>
+    </div>
+    <div class="statusInfo">
+      <h1>{{ flight.flight.iata }}</h1>
+      <h2>{{ flight.flight_status }}</h2>
+      <h3>{{ flight.flight_date }}</h3>
+    </div>
+    <div class="arivalInfo">
+      <h1>{{ flight.arrival.iata }}</h1>
+      <p>{{ flight.arrival.airport }}</p>
+      <p>{{ flight.arrival.scheduled }}</p>
+      <p>Gate: {{ flight.arrival.gate }}</p>
+    </div>
+  </article>
+</template>
+
+<style>
+.flightCard {
+  display: flex;
+  justify-content: space-between;
+
+  padding: 20px;
+  margin-bottom: 10px;
+}
+.statusInfo {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: center;
+}
+.arivalInfo {
+  text-align: right;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,4 @@
+<template>
+  <input type="text" placeholder="Search for Flights">
+  <button>Search</button>
+</template>
